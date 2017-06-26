cdf17a9b3611df1d075b082162b86b0e5bc4f9fe
Yan <junhui.yan@nokia.com>
2017-06-26 17:44:21 +0800

    Make SSH simple server more readable.

--- ssh/ssh_server_simple.py
+++ ssh/ssh_server_simple.py
@@ -3,9 +3,11 @@ import socket
 import threading
 from binascii import hexlify
 import paramiko
+import time
 
+PROMPT = '>'
 
-class SSHServer(paramiko.ServerInterface):
+class SSHServerIf(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
 
@@ -22,65 +24,93 @@ class SSHServer(paramiko.ServerInterface):
             return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
 
-    # def check_auth_gssapi_keyex(self, username,
-    #                             gss_authenticated=paramiko.AUTH_FAILED,
-    #                             cc_file=None):
-    #     return paramiko.AUTH_SUCCESSFUL
-
-    # def check_auth_gssapi_with_mic(self, username,
-    #                                gss_authenticated=paramiko.AUTH_FAILED,
-    #                                cc_file=None):
-    #     return paramiko.AUTH_SUCCESSFUL
-
-    # def check_auth_publickey(self, username, key):
-    #     return paramiko.AUTH_SUCCESSFUL
 
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
-port = 2200
 
-paramiko.util.log_to_file('ssh_server_simple.log')
+class SSHServerControler(object):
+    def __init__(self, port=2200, key_file='server_rsa.key'):
+        self.port = port
+        self.host_key = paramiko.RSAKey(filename=key_file)
+        print('Read key: ' + paramiko.py3compat.u(hexlify(self.host_key.get_fingerprint())))
+        self.stop = None
 
-host_key = paramiko.RSAKey(filename='test_rsa.key')
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', self.port))
+        sock.listen(100)
+        print('listening at %d ...' % self.port)
+        client, addr = sock.accept()
+        print 'Client coming: ', addr
 
-print('Read key: ' + paramiko.py3compat.u(hexlify(host_key.get_fingerprint())))
+        with paramiko.Transport(client, gss_kex=False) as transport:
+            transport.add_server_key(self.host_key)
+            server = SSHServerIf()
+            transport.start_server(server=server)
+            self.accept_connection(server, transport)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('', port))
-sock.listen(100)
-print('listening at %d ...' % port)
-client, addr = sock.accept()
-print 'Client coming: ', addr
+    def accept_connection(self, server, transport):
+        with transport.accept(20) as chan:
+            if chan is None:
+                print('no channel.')
+                sys.exit(1)
+            print('Authrized.')
 
-with paramiko.Transport(client, gss_kex=False) as t:
-    t.add_server_key(host_key)
-    server = SSHServer()
-    t.start_server(server=server)
-    with t.accept(20) as chan:
-        if chan is None:
-            print('no channel.')
-            sys.exit(1)
-        print('Authrized.')
+            server.event.wait(10)
+            if not server.event.is_set():
+                print('*** Client never asked for a shell.')
+                sys.exit(1)
 
-        server.event.wait(10)
-        if not server.event.is_set():
-            print('*** Client never asked for a shell.')
-            sys.exit(1)
-
-        chan.send('\r\nWelcome to my server.\r\n')
-        chan.send('>')
-        f = chan.makefile('rU') 
-        cmd = f.readline().strip('\r\n')
-        chan.send('\r\nreceived: ')
-        chan.send(cmd)
-        chan.send('\r\n')
+            chan.send('\r\nWelcome to my server.\r\n')
+            chan.send(PROMPT)
+            self.receive(chan)
 
 
 
+    def receive(self, chan):
+        in_f = chan.makefile('rU')
+        self.stop = False
+        while not self.stop:
+            line = self.receive_line(chan, in_f)
+            # line = in_f.readline().strip('\r\n')
+            print line
+            chan.send('\n\r' + PROMPT)
+            if 'exit' == line.strip('\n\r'):
+                self.stop = True
+                break
+            time.sleep(0.01)
+
+
+    def receive_line(self, chan, in_f):
+        line = ''
+        c = ''
+        while not self.stop and '\n' not in c and '\r' not in c:
+            c = self.receive_c(chan, in_f)
+            line += c
+            time.sleep(0.01)
+        return line
+
+
+    def receive_c(self, chan, in_f):
+        c = in_f.read(1)
+        if c:
+            chan.send(c)
+        return c
+
+    def stop(self):
+        self.stop = True
+
+
+if __name__ == '__main__':
+
+
+    paramiko.util.log_to_file('ssh_server_simple.log')
+    controller = SSHServerControler()
+    controller.run()
 
 
 
